21ba79a7dc8b634c674d8f1e59fd03fb363b662b
fccagou <me@fccagou.fr>
2026-07-28 16:42:12 +0200

    fix(03-multi): bad ip
    Signed-off-by: fccagou <me@fccagou.fr>

--- assets/scenarios/03-multi/03-root.sh
+++ assets/scenarios/03-multi/03-root.sh
@@ -36,7 +36,7 @@ pei "sudo ip addr add 10.0.0.254/24 dev br0"
 pei "sudo ip link set br0 up"
 
 comment "The bridge is now created and its IP is 10.0.0.254" "Before attaching the 2 host_veth to the bridge, we need to unassign their IP" "In a real CNI plugin, the veth_host are creatred without an IP"
-pe "sudo ip addr del 10.0.0.1/24 dev veth-host"
+pe "sudo ip addr del 10.0.0.10/24 dev veth-host"
 pei "sudo ip addr del 10.0.0.20/24 dev veth-host2"
 
 comment "Now, we can attach the 2 interfaces to the bridge"
